edb6aaca886ce5b6d4034250065f3645a18d48c7
NguyenALuy <s3891919@rmit.edu.vn>
2022-12-31 02:02:19 +0700

    change some front-end for the admin page add new songs and new albums

--- muzikland/src/utils/supportFunction.js
+++ muzikland/src/utils/supportFunction.js
@@ -3,6 +3,7 @@ export const filters = [
     { id: 3, name: "Rock", value: "rock" },
     { id: 4, name: "Melody", value: "melody" },
     { id: 5, name: "Karoke", value: "karoke" },
+    { id: 6, name: "POP", value: "pop" },
   ];
   
   export const filterByLanguage = [
